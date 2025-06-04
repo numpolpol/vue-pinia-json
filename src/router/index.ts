7905045c62955ae78996bcc4ba8ffd7ce3b41d76
Numpol Poldongnok <numpol.pol@ascendcorp.com>
2025-06-04 17:53:17 +0700

    fix: correct route path and remove unnecessary semicolons in router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,17 @@
-
-import { type RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { type RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '/vue-pinia-json',
     name: 'Home',
     component: Home,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
